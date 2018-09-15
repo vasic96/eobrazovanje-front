2943ba6668af58ab981391738ca631355bbf4e79
ChewDlaka <kerber996@gmail.com>
2018-09-15 18:56:48 +0200

    Revert "Material angular"
    This reverts commit 679bbb20533e328de9f479552be280ef451b762c.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -11,8 +10,7 @@ import { MaterialModule } from './material.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    MaterialModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
